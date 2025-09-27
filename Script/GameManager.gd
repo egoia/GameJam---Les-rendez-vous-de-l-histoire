2a3a25915056f0e8c46ef0b931dff03b420e13fe
Seraphiel <76918503+Pierre2650@users.noreply.github.com>
2025-09-27 16:45:42 +0200

    Update scene main

--- Script/GameManager.gd
+++ Script/GameManager.gd
@@ -3,6 +3,7 @@ extends Node2D
 enum Jauge { REPRESSION, CONSERVTIVE, REVOLUTIONNARY}
 
 var current_node : StoryNode
+@onready var backgroud: Sprite2D = $Backgroud
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,15 +15,26 @@ func _process(delta: float) -> void:
 	pass
 
 func manage_choice():
+	#recupere info du prochain evenement
 	var choice = current_node as Choix
 	
+	
+func manage_Fork():
+	pass
+	
+
+
 
 func load_consequence_UI(consequence : Evenement):
+	#transition consequence
 	pass
 	
 func load_choice_UI(choix : Choix):
+	#transition  prochain choix?
+	backgroud.texture = choix.decors
 	pass
 	
 func load_end_UI(end : Fin):
+	#
 	pass
 	
