5def725c73f2e9d86299484fbeed2932a58a8e1d
Seraphiel <76918503+Pierre2650@users.noreply.github.com>
2025-09-27 17:47:14 +0200

    s

--- Script/StoryTree/ChoicePossibility.gd
+++ Script/StoryTree/ChoicePossibility.gd
@@ -1,7 +1,8 @@
 
 class_name ChoicePossibility
 extends Resource
-	
+
+@export var next : StoryNode
 @export var text : String
 @export var value : String
 @export var jauge : GameManager.Jauge
--- Script/StoryTree/Choix.gd
+++ Script/StoryTree/Choix.gd
@@ -3,9 +3,4 @@ extends Evenement
 
 
 @export var choix : Array[ChoicePossibility]
-@export var next_possibilities : Array[StoryNode]
 @export var illustration : Texture2D
-
-
-func getPossibility(i):
-	return next_possibilities[i]
