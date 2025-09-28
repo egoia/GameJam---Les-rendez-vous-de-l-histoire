f528fe5a15d7e3bdd2587cbb7ba824fb247835f0
egoia <elyas.saidoune@gmail.com>
2025-09-28 12:58:28 +0200

    need polish and fixes

--- Script/dj.gd
+++ Script/dj.gd
@@ -10,7 +10,7 @@ const MUS_MENU_LOOPED = preload("uid://c7bh60hvx3owo")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play_song(MUS_MENU_LOOPED)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
